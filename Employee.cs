474056d623c0e4f96fe68e9fffb8855517fd89b9
gabrielcairess <gbrlcaires@gmail.com>
2020-01-20 22:53:32 -0300

    adding files to small project

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeInformations
+{
+    class Employee
+    {
+        public string Name;
+        public double Wage, Duty;
+
+        public double FreeWage()
+        {
+            return Wage - Duty;
+        }
+
+        public double IncreaseWage(double percentual)
+        {
+            return FreeWage() + (Wage * (percentual / 100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace EmployeeInformations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+
+            Console.WriteLine("Digite os dados do funcionario: ");
+            Console.Write("Nome:");
+            e.Name = Console.ReadLine();
+            Console.Write("Salario: ");
+            e.Wage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            e.Duty = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: {0}, $ {1}", e.Name.ToString(), e.FreeWage().ToString());
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salario: ");
+            double percentual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados Atualizados: {0}, $ {1}", e.Name, e.IncreaseWage(percentual));
+        }
+    }
+}
